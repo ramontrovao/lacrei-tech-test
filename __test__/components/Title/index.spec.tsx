c0b527e7f1673edf0bb8deca4b1842a448b817da
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-04-22 20:38:56 -0300

    refactor: moved all tests to __test__ folder

---
+++ __test__/components/Title/index.spec.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+
+import { Title } from "@components/Title";
+import { DefaultTheme } from "@themes/DefaultTheme";
+
+describe("Describe title component", () => {
+  it("should be able to render title component", () => {
+    render(
+      <ThemeProvider theme={DefaultTheme}>
+        <Title text="This is a title test" />
+      </ThemeProvider>
+    );
+
+    screen.getByText("This is a title test");
+  });
+});
