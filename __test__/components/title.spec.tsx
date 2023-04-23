c45d6505fe5f62950cf8882baca03e9bbb45e60b
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-04-23 10:37:17 -0300

    refactor: updated the title test structure

--- __test__/components/title.spec.tsx
+++ __test__/components/title.spec.tsx
@@ -6,16 +6,14 @@ import { ThemeProvider } from "styled-components";
 import { Title } from "@components/Title";
 import { DefaultTheme } from "@themes/DefaultTheme";
 
-describe("Describe title component", () => {
-  it("should be able to render title component", () => {
-    render(
-      <ThemeProvider theme={DefaultTheme}>
-        <Title text="This is a title test" />
-      </ThemeProvider>
-    );
+test("should be able to render title component", () => {
+  render(
+    <ThemeProvider theme={DefaultTheme}>
+      <Title text="This is a title test" />
+    </ThemeProvider>
+  );
 
-    const titleElement = screen.getByRole("heading");
+  const titleElement = screen.getByRole("heading");
 
-    expect(titleElement).toHaveTextContent("This is a title test");
-  });
+  expect(titleElement).toHaveTextContent("This is a title test");
 });
